a9820feea6cc2c2a5cb47e8d8a2b8cd97c441d61
joypoddar <joypoddar.sd@gmail.com>
2020-11-18 18:12:47 +0530

    #9 - Run effects only once

---
+++ state-hooks/src/components/HookMouse.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+function HookMouse() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect Called");
+    window.addEventListener("mousemove", logMousePosition);
+  }, []);
+
+  const logMousePosition = (e) => {
+    console.log("Mouse event");
+    setX(e.clientX);
+    setY(e.clientY);
+  };
+  return (
+    <div>
+      Hooks X - {x} y - {y}
+    </div>
+  );
+}
+
+export default HookMouse;
